d120995501b56803202bb1b59e9d84a2de03de9e
xianrenqiuzhou <95110877+xianrenqiuzhou@users.noreply.github.com>
2026-05-05 06:44:18 +0800

    Change default port from 8080 to 8000

--- main.py
+++ main.py
@@ -498,5 +498,5 @@ def config_diagnostic():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", "8080"))
+    port = int(os.getenv("PORT", "8000"))
     uvicorn.run(app, host="0.0.0.0", port=port)
